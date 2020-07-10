7ae8ba2d98c87eed6f890239e04a5aa6d86334ff
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-07-10 09:26:55 +0000

    For loop added to render correctly the list of products on different screen sizes

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+# View for showing all the product in the database
+
+def all_products(request):
+    
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+    
