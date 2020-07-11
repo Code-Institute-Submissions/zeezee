341cb111edeb0f7c87e8ccf077138d77cc70607f
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-07-11 17:26:00 +0000

    Query set, filtering products by searchterm working

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -6,12 +8,24 @@ from .models import Product
 # View for showing all the product in the database
 
 def all_products(request):
-    
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+    '''If the query is empty, so the user haven't enter any search term, they get an error message'''
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            '''Q variable where the name OR the description contain the query, then filter the products'''           
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
@@ -28,5 +42,3 @@ def detail_product(request, product_id):
     }
 
     return render(request, 'products/detail_product.html', context)
-
-    
