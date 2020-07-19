91c090b6d2d6e985e7ccee4b45b447706d7fa405
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-07-19 14:30:13 +0000

    Pages app wired up in urls, and view

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def about(request):
-    """ Rendering contact (about.html) """
+    """ Rendering about (about.html) """
 
     return render(request, 'pages/about.html')
