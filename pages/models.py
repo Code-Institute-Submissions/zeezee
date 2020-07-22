8a996a26a5c49e61f83db8727ca0a83be363b1ad
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-07-22 14:03:08 +0000

    Form added for contact page

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib import admin
+from django import forms
+from .models import Contact
 
-# Create your models here.
+
+class ContactForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+
+class Contact(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+    message = models.TextField(max_length=10) 
+    
+
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+from django.views.generic import FormView
+from .forms import ContactForm
+
 
 # Create your views here.
 
@@ -7,5 +12,23 @@ def about(request, *args, **kwargs):
     return render(request, "pages/about.html", {"page": "about"})
 
 
-def contact(request, *args, **kwargs):
-    return render(request, "pages/contact.html", {"page": "contact"})
+
+class ContactFormView(FormView):
+
+    form_class = ContactForm
+    template_name = "pages/contact.html"
+    success_url = '/email-sent/'
+
+    def form_valid(self, form):
+        message = "{name} / {email} said: ".format(
+            name=form.cleaned_data.get('name'),
+            email=form.cleaned_data.get('email'))
+        message += "\n\n{0}".format(form.cleaned_data.get('message'))
+        send_mail(
+            subject=form.cleaned_data.get('subject').strip(),
+            message=message,
+            from_email='contact-form@myapp.com',
+            recipient_list=[settings.LIST_OF_EMAIL_RECIPIENTS],
+        )
+        form.save()
+        return super(ContactFormView, self).form_valid(form)
