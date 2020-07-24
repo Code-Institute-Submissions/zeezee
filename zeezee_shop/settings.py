7c3327e99f95223686cfdea4c657f49c03cdd4b5
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-07-24 11:18:25 +0000

    Email backend added to settings.py

--- zeezee_shop/settings.py
+++ zeezee_shop/settings.py
@@ -94,9 +94,14 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1 
 
-DEFAULT_FROM_EMAIL = 'andrekrisztina5@gmail.com'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = "andrekrisztina5@gmail.com"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD  =""
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
