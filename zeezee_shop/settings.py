97b2833e344a5123b1eeb2effa8f3eaa801fc670
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-04 18:43:18 +0000

    Crispy form set in main settings.py

--- zeezee_shop/settings.py
+++ zeezee_shop/settings.py
@@ -63,6 +63,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'zeezee_shop.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -79,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+             'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
