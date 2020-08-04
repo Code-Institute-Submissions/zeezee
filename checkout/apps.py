6a02e6d61a3bf5801f4a5c804c280eeef61a3735
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-04 18:08:09 +0000

    Checkout signals and forms added

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
+def ready(self):
+    '''
+    Import signals, so when any line of the order
+    is deleted or updated,
+    the order will be updated as well
+    '''
+
+        import checkout.signals
