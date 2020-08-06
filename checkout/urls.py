b09dab0a09ce4a6652374853a280a1cfcbb3b1e0
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-06 18:56:30 +0000

    Checkout success added to JS and to urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,6 @@ Path for checkout
 '''
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
