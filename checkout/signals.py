d80437cc23fa9f6d0330b9a7b97fa2383d914ddb
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-07 09:42:12 +0000

    Order infos rendered in success page

--- checkout/signals.py
+++ checkout/signals.py
@@ -15,7 +15,7 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update the total when it's saved
     OrderLineItem model, the line of the order
