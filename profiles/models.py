e6653ce0905761c949d78561abed94cde05dac49
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-09 17:03:40 +0000

    Order history added

--- profiles/models.py
+++ profiles/models.py
@@ -14,7 +14,7 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
@@ -34,6 +34,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create a profile or if there is a profile belonging to a user,
     just update it with the adjustment
     """
-    #if created:
-    UserProfile.objects.create(user=instance)
-    #instance.userprofile.save()
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
