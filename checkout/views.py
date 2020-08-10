e1e5d2a9b6cfee84fc095116d043d67d5caadb82
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-10 14:49:03 +0000

    Error caused by missing variable fixed

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
 from products.models import Product
-from bag.contexts import bag_contents
-from django.views.decorators.http import require_POST
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
-
+from bag.contexts import bag_contents
 
 import stripe
 import json
@@ -56,6 +56,7 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
+
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
@@ -63,45 +64,72 @@ def checkout(request):
             order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
             order.save()
-        for item_id, item_data in bag.items():
-            product = Product.objects.get(id=item_id)
-            order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=item_data,
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for quantity in item_data.items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
                     )
-            order_line_item.save()
+                    order.delete()
+                    return redirect(reverse('view_bag'))
 
-            if not bag:
-                messages.error(request, "Your bag is empty")
-                return redirect(reverse('products'))
+            # Save the info to the user's profile if all is well
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
 
-                current_bag = bag_contents(request)
-                total = current_bag['grand_total']
-                stripe_total = round(total * 100)
-                stripe.api_key = stripe_secret_key
-                intent = stripe.PaymentIntent.create(
-                    amount=stripe_total,
-                    currency=settings.STRIPE_CURRENCY,)
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
-                if request.user.is_authenticated:
-                    try:
-                        profile = UserProfile.objects.get(user=request.user)
-                        order_form = OrderForm(initial={
-                            'full_name': profile.user.get_full_name(),
-                            'email': profile.user.email,
-                            'phone_number': profile.default_phone_number,
-                            'country': profile.default_country,
-                            'postcode': profile.default_postcode,
-                            'town_or_city': profile.default_town_or_city,
-                            'street_address1': profile.default_street_address1,
-                            'street_address2': profile.default_street_address2,
-                            'county': profile.default_county,
-                        })
-                    except UserProfile.DoesNotExist:
-                        order_form = OrderForm()
-                else:
-                    order_form = OrderForm()
+        # Attempt to prefill the form with any info the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -115,6 +143,9 @@ def checkout(request):
     }
 
     return render(request, template, context)
+    
+
+    
 
 
 def checkout_success(request, order_number):
