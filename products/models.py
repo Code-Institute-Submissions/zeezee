c624dcaa1ace5cc383b8b948c60876829d6f0341
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-12 15:39:26 +0000

    Image model changed

--- products/models.py
+++ products/models.py
@@ -28,13 +28,13 @@ class Product(models.Model):
     '''Product model with key elements
     Category, sku, image url and image fields are optional
     '''
-    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image1 = models.ImageField(null=True)
-    image2 = models.ImageField(null=True)
+    image1 = models.ImageField(null=True, blank=True)
+    image2 = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
