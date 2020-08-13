ceea5d48e46d644fdbdaccbe47ff1d4a14e02077
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-13 16:30:59 +0000

    Cache control added

--- zeezee_shop/settings.py
+++ zeezee_shop/settings.py
@@ -188,6 +188,10 @@ FREE_DELIVERY_THRESHOLD = 35
 STANDARD_DELIVERY_PERCENTAGE = 8
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'zeezee-bijoux'
     AWS_S3_REGION_NAME = 'us-east-1'
