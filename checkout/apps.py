3487f8f9e2d79ef2eb3c0410d7a6f43ccd47b159
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-17 15:58:49 +0000

    Indentation error fixed in apps.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,11 +4,6 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     name = 'checkout'
 
-    
-def ready(self):
-    '''
-    Import signals, so when any line of the order
-    is deleted or updated,
-    the order will be updated as well
-    '''
-    import checkout.signals
+    def ready(self):
+        import checkout.signals
+        
