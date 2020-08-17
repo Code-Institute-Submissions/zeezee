9f9dbdb8baa40cecff9a943ff1a1f15fd1cbfea8
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-17 16:53:57 +0000

    Navigation and footer color changed

--- checkout/apps.py
+++ checkout/apps.py
@@ -2,8 +2,12 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    ''''
+    Import signals, so when any line of the order
+    is deleted or updated,
+    the order will be updated as well
+    '''
     name = 'checkout'
 
     def ready(self):
         import checkout.signals
-        
