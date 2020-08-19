a14204fef56e9745ec9f34ab03daf134601695bc
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-19 17:26:03 +0000

    Order status for if statement added

--- checkout/views.py
+++ checkout/views.py
@@ -157,7 +157,7 @@ def checkout_success(request, order_number):
         # Attach the user's profile to the order
         order.user_profile = profile
         order.save()
-
+        
         # Save the user's info
         if save_info:
             profile_data = {
