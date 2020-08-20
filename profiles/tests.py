f6d8309f3f1bba455d916e8c37cc1610471b0df0
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-20 12:55:44 +0000

    View test for profile added

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .views import profile, order_history
 
 # Create your tests here.
+class TestProfileView(TestCase):
+
+    def test_profile_view(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed( 'profiles/profile.html')
+
+    def test_order_history_view(self):
+        response = self.client.get('/order_history/<order_number>')
+        self.assertTemplateUsed( 'checkout/checkout_success.html')
