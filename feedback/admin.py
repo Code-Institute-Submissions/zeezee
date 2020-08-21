9cdc9476f7521478f985774c469ff6b675321eb7
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-21 12:12:31 +0000

    Basic model added to feedback

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, Comment
+
 
 # Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email','approved_comment')
--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.CharField(max_length=200)
+    email = models.EmailField()
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approved(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.user
