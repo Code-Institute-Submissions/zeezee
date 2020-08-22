f2680053ae5a1a246888494b57d2b60d53a8ae0d
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-08-22 16:56:46 +0000

    Status added to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -19,7 +19,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', 'original_bag', 'stripe_pid')
+                       'grand_total', 'original_bag', 'stripe_pid', 'status')
 
     fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
@@ -30,7 +30,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total')
+                    'grand_total', 'status')
 
     ordering = ('-date',)
 
